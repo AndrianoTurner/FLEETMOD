0ff10c46ad637a63b9d83bcaf237366508ea150d
AndrianoTurner <danya.shibaev@gmail.com>
2022-02-10 19:37:36 +0300

    Renamed variables

--- FLEETMOD/UpdatePLPawn.cs
+++ FLEETMOD/UpdatePLPawn.cs
@@ -13,34 +13,33 @@ namespace FLEETMOD
 		// This fixes the health issue with death of a player that is the same class
 		// Although rejoining resets max health to base health
         static void Postfix(PLPawn __instance)
-        {
-		
+		{ 
 				if (__instance.GetPlayer() != null && __instance.GetPlayer().GetPlayerID() != -1)
                 {
 					MyVariables.MySurvivalBonus = MyVariables.survivalBonusDict[__instance.GetPlayer().GetPlayerID()];
-						float num10 = 100f;
+						float BaseHealth = 100f;
 						if (__instance.GetPlayer().RaceID == 2)
 						{
-							num10 = 60f;
+							BaseHealth = 60f;
 						}
-						float num11 = num10 + (float)__instance.GetPlayer().Talents[0] * 20f;
-						num11 += (float)__instance.GetPlayer().Talents[57] * 20f;
+						float ModifiedHealth = BaseHealth + (float)__instance.GetPlayer().Talents[0] * 20f;
+						ModifiedHealth += (float)__instance.GetPlayer().Talents[57] * 20f;
 						foreach (PawnStatusEffect pawnStatusEffect5 in __instance.MyStatusEffects)
 						{
 							if (pawnStatusEffect5 != null && pawnStatusEffect5.Type == EPawnStatusEffectType.HEALTH_REGEN)
 							{
-								num11 += 20f;
+								ModifiedHealth += 20f;
 							}
 						}
-						float value2 = num11;
-						if (__instance.GetPlayer().GetClassID() < 5)
+						float value2 = ModifiedHealth;
+						if (__instance.GetPlayer().GetClassID() != -1 && __instance.GetPlayer().GetClassID() < 5)
 						{						
-							num11 +=  MyVariables.MySurvivalBonus * 5f;
+							ModifiedHealth +=  MyVariables.MySurvivalBonus * 5f;
 						}
-						if (__instance.MaxHealth != num11)
+						if (__instance.MaxHealth != ModifiedHealth)
 						{
-							__instance.Health = __instance.Health / __instance.MaxHealth * num11;
-							__instance.MaxHealth = num11;
+							__instance.Health = __instance.Health / __instance.MaxHealth * ModifiedHealth;
+							__instance.MaxHealth = ModifiedHealth;
 							__instance.MaxHealth_Normal = value2;
 						}
 				}
