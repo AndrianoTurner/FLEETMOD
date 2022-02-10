0c64dc991f10eb1c79a6988cd95ef62d8a4c63e6
AndrianoTurner <danya.shibaev@gmail.com>
2022-02-10 19:35:12 +0300

    Implement highrollers for other players?

---
+++ FLEETMOD/HighRollersOnLiarsDiceGameWin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace FLEETMOD
+{
+    /*[HarmonyPatch(typeof(PLHighRollersShipInfo), "OnLiarsDiceGameWin")]
+    internal class HighRollersOnLiarsDiceGameWin
+    {
+        public static void Postfix(PLHighRollersShipInfo __instance,PLPlayer player, PLLiarsDiceGame game)
+        {
+			if (player != null)
+			{
+				if (MyVariables.IsInFleet(player.StartingShip.ShipID) && Array.IndexOf<PLLiarsDiceGame>(__instance.SmallGames, game) != -1)
+				{
+					__instance.CrewChips++;
+				}
+				
+			}
+		}
+    }
+	*/
+}
